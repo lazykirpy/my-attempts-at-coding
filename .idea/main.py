7c3afdfc9acceb1694f5177e33eb21b4ed0d5c56
lazykirpy <woofwofthedog@gmail.com>
2024-08-11 15:13:59 +0300

    is it a prime?

--- .idea/main.py
+++ .idea/main.py
@@ -1,1 +1,16 @@
-print("hello, world!")
+print("input a number, I'll check if it's a prime number or not!")
+
+def prime(num):
+    s = 2
+    while s != num:
+        if num % s == 0:
+            print("is NOT a prime")
+            break
+        elif s == num-1:
+            print("is a prime")
+            break
+        else:
+            s = s + 1
+while True:
+    i = int(input())
+    prime(i)
